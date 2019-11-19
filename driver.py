d3b8dfd688c4a96297c1e98d11e354f089f7cfae
Jesse Tutor <jessetutor97@gmail.com>
2019-11-18 20:40:21 -0600

    Adding code to control solenoid

--- driver.py
+++ driver.py
@@ -4,17 +4,31 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 import numpy as np
 import time
+import RPi.GPIO as GPIO
 
 pga = 0256
 sps = 860
 adc = ADS1x15(ic=0x01)
 adc.startContinuousConversion(0,pga,sps)
 data = []
+
+# Setup GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(24,GPIO.OUT)
+
 print("Go!")
 N = sps * 10
+
+# Read sensor data
 for num in range(N):
 	data.append(adc.getLastConversionResults())
 	time.sleep(5 / N)
+        if num == (N // 4):
+            GPIO.output(24,GPIO.HIGH) # GPIO High
+            time.sleep(0.05)
+            GPIO.output(24,GPIO.LOW)  # GPIO Low
+
 adc.stopContinuousConversion()
 print("Done")
 
@@ -31,7 +45,6 @@ plt.plot(smooth_data, 'b-')
 plt.show()
 
 T = 1.0 / N
-x = np.linspace(0.0, N*T, N)
 yf = fft(data)
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
