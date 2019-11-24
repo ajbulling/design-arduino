253e998e5aa4946cec285948f32da449442af83d
Jesse Tutor <jessetutor97@gmail.com>
2019-11-24 13:06:10 -0600

    Adding new sensor output

--- driver.py
+++ driver.py
@@ -3,19 +3,21 @@ from scipy.fftpack import fft
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 import numpy as np
+import scipy as sp
 import time
 import RPi.GPIO as GPIO
 
-pga = 0256
-sps = 860
+pga = 6144
+sps = 3300
 adc = ADS1x15(ic=0x01)
-adc.startContinuousConversion(0,pga,sps)
+adc.startContinuousConversion(1,pga,sps)
 data = []
 
 # Setup GPIO pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(24,GPIO.OUT)
+GPIO.output(24,GPIO.LOW)  # GPIO Low
 
 print("Go!")
 N = sps * 10
@@ -23,26 +25,27 @@ N = sps * 10
 # Read sensor data
 for num in range(N):
 	data.append(adc.getLastConversionResults())
-	time.sleep(5 / N)
+	time.sleep(10 / N)
         if num == (N // 4):
             GPIO.output(24,GPIO.HIGH) # GPIO High
             time.sleep(0.05)
             GPIO.output(24,GPIO.LOW)  # GPIO Low
+            print("Hit!")
 
 adc.stopContinuousConversion()
 print("Done")
 
 
-filt_N = 3 #Filter order
-Wn = 0.1  #cutoff frequency
-B, A = signal.butter(filt_N, Wn, output='ba')
-smooth_data = signal.filtfilt(B,A, data)
+#filt_N = 3 #Filter order
+#Wn = 0.1  #cutoff frequency
+#B, A = signal.butter(filt_N, Wn, output='ba')
+#smooth_data = signal.filtfilt(B,A, data)
 t = np.arange(N)
 plt.plot(t, data)
 plt.show()
 
-plt.plot(smooth_data, 'b-')
-plt.show()
+#plt.plot(smooth_data, 'b-')
+#plt.show()
 
 T = 1.0 / N
 yf = fft(data)
@@ -51,3 +54,10 @@ plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
 plt.grid()
 plt.show()
 
+#Unhelpful so far
+psd = np.abs(yf)**2
+fftFreq = sp.fftpack.fftfreq(len(psd), 1. / 3300)
+usableFreq = fftFreq > 0
+plt.plot(fftFreq[usableFreq], 10* np.log(psd[usableFreq]))
+plt.grid()
+plt.show()
