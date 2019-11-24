d6e30a1295251831a27d3e70530a5c9cd28ce864
Jesse Tutor <jessetutor97@gmail.com>
2019-11-24 17:31:46 -0600

    Trimming down measurement time

--- driver.py
+++ driver.py
@@ -7,10 +7,12 @@ import scipy as sp
 import time
 import RPi.GPIO as GPIO
 
-pga = 6144
-sps = 3300
+gains = [6144, 4096, 2048, 1024, 512, 256]
+samples = [3300, 2400, 1600, 920, 490, 250, 128]
+pga = gains[0]
+sps = samples[0]
 adc = ADS1x15(ic=0x01)
-adc.startContinuousConversion(1,pga,sps)
+adc.startContinuousConversion(3,pga,sps)
 data = []
 
 # Setup GPIO pins
@@ -20,44 +22,67 @@ GPIO.setup(24,GPIO.OUT)
 GPIO.output(24,GPIO.LOW)  # GPIO Low
 
 print("Go!")
-N = sps * 10
+N = sps
+
+
+GPIO.output(24,GPIO.HIGH) # GPIO High
+#time.sleep(0.05)
+GPIO.output(24,GPIO.LOW)  # GPIO Low
+print("Hit!")
 
 # Read sensor data
+time.sleep(0.25)
 for num in range(N):
 	data.append(adc.getLastConversionResults())
-	time.sleep(10 / N)
+	#time.sleep(5 / N)
+        '''
         if num == (N // 4):
             GPIO.output(24,GPIO.HIGH) # GPIO High
-            time.sleep(0.05)
+            time.sleep(0.1)
             GPIO.output(24,GPIO.LOW)  # GPIO Low
             print("Hit!")
+            '''
 
 adc.stopContinuousConversion()
 print("Done")
 
 
-#filt_N = 3 #Filter order
-#Wn = 0.1  #cutoff frequency
-#B, A = signal.butter(filt_N, Wn, output='ba')
-#smooth_data = signal.filtfilt(B,A, data)
-t = np.arange(N)
-plt.plot(t, data)
+filt_N = 1 #Filter order
+Wn = 0.5  #cutoff frequency
+B, A = signal.butter(filt_N, Wn, output='ba')
+smooth_data = signal.filtfilt(B,A, data[:N//4])
+t = np.arange(N//4)
+plt.plot(t, data[:N//4])
 plt.show()
 
-#plt.plot(smooth_data, 'b-')
-#plt.show()
+plt.plot(smooth_data, 'b-')
+plt.show()
 
 T = 1.0 / N
-yf = fft(data)
-xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
-plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
+yf = fft(smooth_data)
+xf = np.linspace(0.0, 1.0/(2.0*T), N//4)
+#yf = np.sin(50.0*2.0*np.pi*xf)
+#yf = fft(yf)
+l2d = plt.plot(xf, 2.0/N * np.abs(yf[0:N//4]))
 plt.grid()
 plt.show()
 
+#Find fundamental frequency
+max_freq = 0
+x_index = 0
+for i in range(200, 800):
+    if l2d[0].get_ydata()[i] > max_freq:
+        max_freq = l2d[0].get_ydata()[i]
+        x_index = i
+
+print("Frequency is " + str(x_index))
+
 #Unhelpful so far
+
 psd = np.abs(yf)**2
 fftFreq = sp.fftpack.fftfreq(len(psd), 1. / 3300)
 usableFreq = fftFreq > 0
 plt.plot(fftFreq[usableFreq], 10* np.log(psd[usableFreq]))
 plt.grid()
-plt.show()
+
+#plt.show()
